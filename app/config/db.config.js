d0b7ef7ec22e37f7f4484f835b0f636441eec469
NihalKPatel <70577135+NihalKPatel@users.noreply.github.com>
2022-09-04 22:26:15 +1200

    Revert "added readme and delete function"
    This reverts commit 4ccadfc5bdcea045c089ffc8bc0f82dfdcd39f31.

--- app/config/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    database: "dd10l8je36ufka",
-    username: "wtwrsdhrmrlmke",
-    password: "fe0182b96266cd464b86b79027d0775195b730f537f5e7eb31a917d997f2529e",
-    host: "ec2-54-204-241-136.compute-1.amazonaws.com",
-    port: 5432,
-    dialect: "postgres"
-};
